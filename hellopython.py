481589065620ea11c06f41ce2169621194a6bad6
jihfan <jiffcodes@gmail.com>
2019-07-14 20:17:05 -0600

    First commit

---
+++ hellopython.py
@@ -0,0 +1,30 @@
+import random
+import sys
+import os
+
+print ("Hello World")
+
+'''
+multiline
+'''
+
+name = "My Name Is"
+print(name)
+
+pythonDataTypes = ['Numbers', 'Strings', 'Lists', 'Tuples','Dictionaries']
+arithmaticOperators = {
+    add: '+',
+    sub: '-',
+    mult: '*',
+    div: '/',
+    mod: '%, which returns the remainder when divided',
+    exponent: '**',
+    floorDiv: '//, divide and discard remainder and round down'
+}
+
+print(5//2)
+print(5**2)
+
+quote = "\"Always use back slash"
+    
+
